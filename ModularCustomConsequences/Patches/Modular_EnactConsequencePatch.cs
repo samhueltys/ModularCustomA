36aefe15a73774e702a0303b9a38796ea58e21eb
Titouan BONDIER <Titouan BONDIER@IA89-ASH290>
2026-03-27 09:56:55 +0100

    final countdown

--- ModularCustomConsequences/Patches/Modular_EnactConsequencePatch.cs
+++ ModularCustomConsequences/Patches/Modular_EnactConsequencePatch.cs
@@ -2,32 +2,74 @@ using HarmonyLib;
 using Lethe.Patches;
 using ModularSkillScripts;
 using System.Text.RegularExpressions;
+using Il2CppSystem.Collections.Generic;
 using MTCustomScripts;
-using System.Linq;
 
 internal class Modular_EnactConsequence
 {
+
+    private static readonly Regex matchReg = Main.Instance.replaceStringRegex;
+
+
     [HarmonyPatch(typeof(ModularSA), "Consequence")]
     [HarmonyPrefix, HarmonyPriority(Priority.VeryHigh)]
     public static bool Prefix_ModularSA_Consequence(ref string section, ModularSA __instance)
     {
         try
         {
-            section = Regex.Replace(section, @"\[([^\[\]]*)\]", match =>
+            section = matchReg.Replace(section, match =>
             {
                 string matchValue = match.Groups[1].Value;
+                string sourceType = match.Groups[2].Success ? match.Groups[2].Value : null;
 
-                object foundData = MTCustomScripts.Main.GetCustomMTData(__instance.modsa_unitModel.Pointer.ToInt64(), matchValue);
-                return (foundData != null) ? foundData.ToString() : matchValue;
+                string outValue = Main.GetCustomMTData(__instance.modsa_unitModel.Pointer.ToInt64(), match.Groups[1].Value, sourceType);
+                return outValue != null ? outValue : match.Groups[0].Value;
             });
         }
-        catch (System.Exception ex)
-        {
-            MainClass.Logg.LogInfo(ex);
-        }
+        catch (System.Exception ex) { MainClass.Logg.LogInfo(ex); }
 
         return true;
     }
+
+    [HarmonyPatch(typeof(ModularSA), nameof(ModularSA.GetTargetModelList))]
+    [HarmonyPostfix, HarmonyPriority(Priority.Low)]
+    public static void Postfix_ModularSA_GetTargetModelList(string param, ModularSA __instance, ref List<BattleUnitModel> __result)
+    {
+        string paramCopy = param.ToLower();
+        BattleObjectManager objectManager = SingletonBehavior<BattleObjectManager>.Instance;
+
+        if (paramCopy.Contains("char"))
+        {
+            int notPosition = paramCopy.IndexOf("char") + 2;
+            string mode = (paramCopy.Length >= notPosition) ? (paramCopy[notPosition - 1] + paramCopy[notPosition]).ToString() : "0";
+
+            _ = int.TryParse(mode.TrimEnd('s'), out int charID);
+
+            BattleUnitModel charUnit = objectManager.GetModelByCharacterID(charID);
+            if (charUnit != null && !__result.Contains(charUnit)) __result.Add(charUnit);
+        }
+
+        if (paramCopy.Contains("not"))
+        {
+            List<BattleUnitModel> notList = new();
+
+            int notPosition = paramCopy.IndexOf("not") + 1;
+            char mode = (paramCopy.Length >= notPosition) ? paramCopy[notPosition] : '0';
+
+            notList = mode switch
+            {
+                '1' => objectManager.GetAliveList(false, UNIT_FACTION.PLAYER),
+                '2' => objectManager.GetAliveList(true, UNIT_FACTION.PLAYER),
+                '3' => objectManager.GetAliveList(false, UNIT_FACTION.ENEMY),
+                '4' => objectManager.GetAliveList(true, UNIT_FACTION.ENEMY),
+                _ => objectManager.GetModelList(),
+            };
+            for (int i = 0; i < __result.Count; i++) if (notList.Contains(__result[i])) notList.Remove(__result[i]);
+
+            __result = notList;
+        }
+    }
+
 
     [HarmonyPatch(typeof(ModularSA), "ProcessBatch")]
     [HarmonyPrefix, HarmonyPriority(Priority.VeryHigh)]
